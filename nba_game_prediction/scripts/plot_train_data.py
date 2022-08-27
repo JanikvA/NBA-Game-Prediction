2e98f2943b71bcc19e0aca4fe0ef7b7ac2c0f24f
Janik von Ahnen <j.v.ahnen@gmx.de>
2022-08-27 16:06:49 +0200

    first plots

--- nba_game_prediction/scripts/plot_train_data.py
+++ nba_game_prediction/scripts/plot_train_data.py
@@ -1,31 +1,55 @@
+import math
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
 from nba_game_prediction import config_modul
 
 
-def read_data(train_data_path):
-    return pd.read_csv(train_data_path)
-
-
 def main(config):
-    train_data = read_data(config["train_data_path"])
-    # plt.matshow(train_data.corr())
-    train_data["ELO_difference"] = train_data.apply(
-        lambda row: row["HOME_ELO"] - row["AWAY_ELO"], axis=1
+    train_data = pd.read_csv(config["train_data_path"])
+    train_data = train_data.drop(
+        [
+            "GAME_ID",
+            "SEASON_ID",
+            "AWAY_TEAM_NAME",
+            "HOME_TEAM_NAME",
+            "GAME_DATE",
+            "AWAY_is_back_to_back",
+            "HOME_is_back_to_back",
+        ],
+        axis=1,
     )
-    print(train_data.keys())
-    exit(1)
-    # sns.scatterplot(
-    #     data=train_data, x="ELO_difference", y="HOME_PLUS_MINUS_10G", hue="HOME_WL"
-    # )
     # sns.heatmap(train_data.corr(), vmin=-1, vmax=1, annot=True)
-    # sns.heatmap(train_data.corr(), vmin=-1, vmax=1, annot=False)
-    # sns.scatterplot(data=train_data, x="HOME_ELO", y="AWAY_ELO", hue="HOME_WL")
-    # sns.kdeplot( data=train_data, x="HOME_ELO", y="AWAY_ELO", hue="HOME_WL")
-    # sns.pairplot(data=train_data)
-    plt.tight_layout()
-    plt.show()
+    # sns.pairplot(data=train_data, hue="HOME_WL", diag_kws={"common_norm":False})
+    bins = [0, 0.2, 0.3, 0.4, 0.45, 0.5, 0.55, 0.6, 0.7, 0.8, 1]
+    pd.DataFrame()
+    x_data = []
+    y_pred_data = []
+    y_actual_data = []
+    y_err = []
+    ratio = []
+    for n, lower_bound in enumerate(bins):
+        if n + 1 == len(bins):
+            break
+        b1 = train_data.loc[
+            (train_data["HOME_ELO_winprob"] > lower_bound)
+            & (train_data["HOME_ELO_winprob"] < bins[n + 1])
+        ]
+        bin_center = lower_bound + (bins[n + 1] - lower_bound) / 2
+        x_data.append(bin_center)
+        y_pred_data.append(b1["HOME_ELO_winprob"].mean())
+        y_actual_data.append(b1["HOME_WL"].mean())
+        W = b1["HOME_WL"].count() * b1["HOME_WL"].mean()
+        L = b1["HOME_WL"].count() * (1 - b1["HOME_WL"].mean())
+        # from error propagation of W/(W+L)
+        unc = math.sqrt(
+            L**2 * math.sqrt(W) / (W + L) ** 4 + W**2 * math.sqrt(L) / (L + W) ** 4
+        )
+        y_err.append(unc / y_pred_data[-1])
+        ratio.append(y_actual_data[-1] / y_pred_data[-1])
+    plt.errorbar(x=x_data, y=ratio, yerr=y_err, color="black")
+    plt.savefig("test.png")
 
 
 if __name__ == "__main__":
